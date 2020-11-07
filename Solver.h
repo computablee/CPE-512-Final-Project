735d6d291e84babc13f391cea4b86cc8d2b79103
Lane <iamaperson620@gmail.com>
2020-11-06 23:01:30 -0600

    commenting

--- Solver.h
+++ Solver.h
@@ -2,6 +2,7 @@
 #include "Pyra.h"
 #include <stdbool.h>
 
+//see Solver.c for descriptions
 void solvePuzzle(Puzzle pyra, int maxMoves, const char* sidesUsed);
 char* createAssociations(const char* sidesUsed);
 void convertMoves(char* numb, const char* associations);
