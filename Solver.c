95170c28491943a700027f195097a43af0675b5e
Lane <iamaperson620@gmail.com>
2020-11-06 23:01:24 -0600

    fixed a bug with exhausting the solution space

--- Solver.c
+++ Solver.c
@@ -9,67 +9,74 @@
 //#define DEBUG
 //#define HARDDEBUG
 
+//this function creates the association between integers of base N and moves, where N is the total number of moves that can be applied to the puzzle (inclusive of both CW and CCW rotations)
 char* createAssociations(const char* sidesUsed)
 {
 	size_t len = sizeof(char) * strlen(sidesUsed) * 2 + 1;
 	char* res = (char*)malloc(len);
 	if (res == NULL)
-		return NULL;
+		return NULL; //check for bad allocation
 	
 	for (char c = sidesUsed[0], i = 0; (unsigned int)i < strlen(sidesUsed); c = sidesUsed[(int)++i])
 	{
-		res[2 * i] = c;
-		res[2 * i + 1] = (char)toupper(c);
+		res[2 * i] = c; //set the nth element of res to the CW move
+		res[2 * i + 1] = (char)toupper(c); //set the nth+1 element to the CCW move
 	}
 
-	res[2 * strlen(sidesUsed)] = 0;
+	res[2 * strlen(sidesUsed)] = 0; //null-terminate so we can do strlen later
 
-	return res;
+	return res; //return the pointer
 }
 
+//this converts a base N integer to a sequence of moves to apply to the pyraminx, based on the associations string
 void convertMoves(char* numb, const char* associations)
 {
 	for (int i = 0; (unsigned int)i < strlen(numb); i++)
 	{
-		char chr[2] = { numb[i], 0 };
-		numb[i] = associations[atoi(chr)];
+		char chr[2] = { numb[i], 0 }; //create a string containing the single character of the place in *numb to convert
+		numb[i] = associations[atoi(chr)]; //convert to a move based on associations (note: itoa is not in gcc, but atoi is??)
 	}
 }
 
+//checks if the puzzle is solved
 bool solved(Puzzle pyra)
 {
-	return pyra.f.top.color == pyra.f.left.color &&
+	return pyra.f.top.color == pyra.f.left.color && //check if the front face is a solid color
 		pyra.f.top.color == pyra.f.right.color &&
 		pyra.f.top.color == pyra.f.left_e.color &&
 		pyra.f.top.color == pyra.f.right_e.color &&
 		pyra.f.top.color == pyra.f.bottom_e.color &&
-		pyra.l.top.color == pyra.l.left.color &&
+		pyra.l.top.color == pyra.l.left.color && //check if the left face is a solid color
 		pyra.l.top.color == pyra.l.right.color &&
 		pyra.l.top.color == pyra.l.left_e.color &&
 		pyra.l.top.color == pyra.l.right_e.color &&
 		pyra.l.top.color == pyra.l.bottom_e.color &&
-		pyra.r.top.color == pyra.r.left.color &&
+		pyra.r.top.color == pyra.r.left.color && //check if the right face is a solid color
 		pyra.r.top.color == pyra.r.right.color &&
 		pyra.r.top.color == pyra.r.left_e.color &&
 		pyra.r.top.color == pyra.r.right_e.color &&
 		pyra.r.top.color == pyra.r.bottom_e.color &&
-		pyra.d.top.color == pyra.d.left.color &&
+		pyra.d.top.color == pyra.d.left.color && //check if the down face is a solid color
 		pyra.d.top.color == pyra.d.right.color &&
 		pyra.d.top.color == pyra.d.left_e.color &&
 		pyra.d.top.color == pyra.d.right_e.color &&
 		pyra.d.top.color == pyra.d.bottom_e.color;
 }
 
+//converts the internal string used to represent an algorithm to a nice pretty formatted user-readable string
+//this uses standard WCA algorithmic notation for pyraminxes
 void outputSolution(const char* solution)
 {
+	//create a buffer
 	const size_t size = sizeof(char) * strlen(solution) * 3 + 1;
 	char* outp = (char*)malloc(size);
 	if (outp == NULL)
-		return;
-	outp[0] = 0;
+		return; //if the allocation failed, return
+	outp[0] = 0; //set to null-terminated
 
 	for (int i = 0; (unsigned int)i < strlen(solution); i++)
 	{
+		//convert internal moves to more human-readable moves
 		switch(solution[i])
 		{
 		case 'l':
@@ -100,14 +107,19 @@ void outputSolution(const char* solution)
 			break;
 		}
 	}
+	
+	//print the algorithm
 	printf("Found solution: %s\n", outp);
-	free(outp);
+	free(outp); //free
 }
 
+//performs a solve based on a puzzle and a solve string
 inline void performSolve(char* solvestr, Puzzle* temppyra)
 {
+	//iterate through the solve string
 	for (int i = 0; (unsigned int)i < strlen(solvestr); i++)
 	{
+		//perform the moves
 		switch (solvestr[i])
 		{
 		case 'l':
@@ -139,10 +151,18 @@ inline void performSolve(char* solvestr, Puzzle* temppyra)
 		}
 	}
 
+	//if solved, output solution in a human-readable format
 	if (solved(*temppyra))
 		outputSolution(solvestr);
 }
 
+//converts an integer to a base N integer
+//this works in 3 conditions:
+//1. you don't mind the output being in reverse order (so 10 to base 8 is 21 not 12)
+//2. you can guarantee that the base is <10
+//3. input is positive
+//I meet all of these conditions in this program, so this algorithm is very concise
+//comments omitted because I feel it's a relatively self-explanatory base conversion function that needs no further explanation
 void convertBase(int numb, char* outp, int base)
 {
 	int pos = 0;
@@ -162,51 +182,72 @@ void convertBase(int numb, char* outp, int base)
 	outp[pos] = 0;
 }
 
+//solve the puzzle
 void solvePuzzle(Puzzle pyra, int maxMoves, const char* sidesUsed)
 {
+	//create the array of associations
 	char* associations = createAssociations(sidesUsed);
 	
-#pragma omp parallel firstprivate(pyra) shared(maxMoves, sidesUsed, associations)
+	//spawn threads
+	//we create a local copy of pyra for each thread so they're not fighting over memory, but everything else is shared
+	#pragma omp parallel firstprivate(pyra) shared(maxMoves, sidesUsed, associations)
 	{
-		bool in_length = true;
-		int turn = omp_get_thread_num();
-		int oldlen = 0;
+		bool in_length = true; //boolean to check if we're under or equal to the maximum algorithm length
+		int turn = omp_get_thread_num(); //get the thread number. this will be used to calculate an algorithm
+		int oldlen = 0; //old algorithm length (for debugging purposes)
+		
+		//while we're under or equal to maximum algorithm length
 		while (in_length)
 		{
+			//create a temporary pyraminx to perform an algorithm on
 			Puzzle temppyra = pyra;
+			//convert the turn variable to an base N integer (where N is the number of possible turns we can make)
 			char baseXstr[100];
 			convertBase(turn, baseXstr, (int)strlen(sidesUsed) * 2);
 			
 #ifdef DEBUG
+			//if we've moved from n-length algorithms to n+1-length algorithms, alert the user that this thread is done with n-length algorithms
 			if (oldlen < (int)strlen(baseXstr))
 			{
 				printf("Thread %d finished searching moves of length %d\n", omp_get_thread_num(), oldlen);
 				oldlen = (int)strlen(baseXstr);
 			}
 #endif
+			//if we've exceeded maximum algorithm length, break
+			//this use of a flag/continue is to avoid a goto
+			//while I honestly would much prefer a goto, I've had professors in the past criticize me for even very conservative goto use
+			//want to avoid the risk of getting counted off
 			if (strlen(baseXstr) > (size_t)maxMoves)
 			{
 				in_length = false;
 				continue;
 			}
 			
+			//convert the base N string to a string of moves
 			convertMoves(baseXstr, associations);
 
 #ifdef HARDDEBUG
+			//if we're SERIOUSLY debugging this program, output the algorithm we're trying
 			printf("Trying algorithm: %s\n", baseXstr);
 #endif
 
+			//perform the solve
 			performSolve(baseXstr, &temppyra);
-			if (baseXstr[strlen(baseXstr) - 1] == 'L')
+			//this condition is because base N integers can NEVER start with a 0 (or end in a 0, since my base converter outputs mirrored)
+			//because of this, algorithms can never end in certain moves (the move associated with the number 0)
+			//this is a hack to make sure we're exhausting the totality of the solution space
+			if (baseXstr[strlen(baseXstr) - 1] == associations[1])
 			{
 				temppyra = pyra;
-				baseXstr[strlen(baseXstr) - 1] = 'l';
+				baseXstr[strlen(baseXstr) - 1] = associations[0];
 				performSolve(baseXstr, &temppyra);
 			}
 
+			//add the number of threads
 			turn += omp_get_num_threads();
 		}
 	}
 
+	//free the associations string
 	free(associations);
 }
