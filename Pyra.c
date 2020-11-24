4a6e5215c8a2a2cad94522b2c65e54d239d17ede
Lane <iamaperson620@gmail.com>
2020-11-23 19:55:33 -0600

    moved the inline functions to the .h, optimized X' moves

--- Pyra.c
+++ Pyra.c
@@ -1,23 +1,5 @@
 #include "Pyra.h"
 
-//performs a rotation of 3 edges
-inline void rotateEdges(Edge* edge1, Edge* edge2, Edge* edge3)
-{
-	const Edge temp = *edge1;
-	*edge1 = *edge2;
-	*edge2 = *edge3;
-	*edge3 = temp;
-}
-
-//performs a rotation of 3 centers
-inline void rotateCenters(Center* center1, Center* center2, Center* center3)
-{
-	const Center temp = *center1;
-	*center1 = *center2;
-	*center2 = *center3;
-	*center3 = temp;
-}
-
 //performs an L
 void l(Puzzle* pyra)
 {
@@ -27,12 +9,11 @@ void l(Puzzle* pyra)
 }
 
 //performs an L'
-//NOTE: could make this more efficient by not calling l(Puzzle*) twice but instead actually performing a *real* L'
-//however, it is time consuming for me to define rotations, so I'm being lazy and doing this instead
-//this note omitted in other X' move functions
 void lp(Puzzle* pyra)
 {
-	l(pyra); l(pyra);
+	rotateEdges(&pyra->l.bottom_e, &pyra->f.left_e, &pyra->d.right_e);
+	rotateEdges(&pyra->l.right_e, &pyra->f.bottom_e, &pyra->d.left_e);
+	rotateCenters(&pyra->l.right, &pyra->f.left, &pyra->d.top);
 }
 
 //performs a U
@@ -46,7 +27,9 @@ void u(Puzzle* pyra)
 //performs a U'
 void up(Puzzle* pyra)
 {
-	u(pyra); u(pyra);
+	rotateEdges(&pyra->r.left_e, &pyra->f.left_e, &pyra->l.left_e);
+	rotateEdges(&pyra->r.right_e, &pyra->f.right_e, &pyra->l.right_e);
+	rotateCenters(&pyra->r.top, &pyra->f.top, &pyra->l.top);
 }
 
 //performs an R
@@ -60,7 +43,9 @@ void r(Puzzle* pyra)
 //performs an R'
 void rp(Puzzle* pyra)
 {
-	r(pyra); r(pyra);
+	rotateEdges(&pyra->f.bottom_e, &pyra->r.left_e, &pyra->d.bottom_e);
+	rotateEdges(&pyra->f.right_e, &pyra->r.bottom_e, &pyra->d.right_e);
+	rotateCenters(&pyra->f.right, &pyra->r.left, &pyra->d.right);
 }
 
 //performs a B
@@ -74,5 +59,7 @@ void b(Puzzle* pyra)
 //performs a B'
 void bp(Puzzle* pyra)
 {
-	b(pyra); b(pyra);
+	rotateEdges(&pyra->r.bottom_e, &pyra->l.left_e, &pyra->d.left_e);
+	rotateEdges(&pyra->r.right_e, &pyra->l.bottom_e, &pyra->d.bottom_e);
+	rotateCenters(&pyra->r.right, &pyra->l.left, &pyra->d.left);
 }
--- Pyra.h
+++ Pyra.h
@@ -48,3 +48,21 @@ void r(Puzzle* pyra);
 void rp(Puzzle* pyra);
 void b(Puzzle* pyra);
 void bp(Puzzle* pyra);
+
+//performs a rotation of 3 edges
+inline void rotateEdges(Edge* edge1, Edge* edge2, Edge* edge3)
+{
+	const Edge temp = *edge1;
+	*edge1 = *edge2;
+	*edge2 = *edge3;
+	*edge3 = temp;
+}
+
+//performs a rotation of 3 centers
+inline void rotateCenters(Center* center1, Center* center2, Center* center3)
+{
+	const Center temp = *center1;
+	*center1 = *center2;
+	*center2 = *center3;
+	*center3 = temp;
+}
